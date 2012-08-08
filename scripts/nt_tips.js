9519cf5788ead17b3ef9dd9e1eb393d5b689c1fd
ospalh <ospalh@gmail.com>
2012-08-08 11:07:13 +0200

    Write up the note type stuff.

---
+++ scripts/nt_tips.js
@@ -0,0 +1,20 @@
+
+
+
+
+$(document).ready(function(){
+
+    $('#once').qtip({
+        content: {
+            text:'Or rather, once for each version of the add-on.\
+See below.',
+        },
+        style: {
+            classes: 'ui-tooltip-rounded ui-tooltip-shadow',
+        },
+        show: {
+	    solo: true
+	}
+    });
+
+});
